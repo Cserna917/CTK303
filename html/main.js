ab3dfa5761cbc573fc4d3b84991edff5a5584b48
Cserna917 <bcserna@ilstu.edu>
2020-02-02 23:53:04 -0600

    nice

---
+++ html/main.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+
+  "use strict"
+
+$('.slider').ripples({
+  dropRadius: 10,
+  perturbance: 0.01,
+
+});
+
+});
+//copyright https://www.youtube.com/watch?v=0ZQcDHLkqYQs
